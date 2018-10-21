977532f4bf1f91aa9665eb84e93819468e1d057d
Janmejay S Purohit <janmejayspurohit@gmail.com>
2018-10-21 15:22:00 +0530

    The Unity Code for Google VR

---
+++ Assets/Script/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewModel : MonoBehaviour
+{
+			public void Button_Click(string scenename)
+			{
+        Application.LoadLevel(scenename);
+			}
+}
